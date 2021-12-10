b3bbc9c65b24a86690a94038b6865bb41934c79f
Miklós <miketm001@gmail.com>
2021-12-10 17:59:43 +0200

    m delete color scheme

--- kalaka/App.tsx
+++ kalaka/App.tsx
@@ -1,18 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import Navigation from './navigation';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { NativeBaseProvider } from 'native-base';
-import Toast from 'react-native-toast-message';
+import useCachedResources from "./hooks/useCachedResources";
+import Navigation from "./navigation";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { NativeBaseProvider } from "native-base";
+import Toast from "react-native-toast-message";
 
 export default function App() {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   const isLoadingComplete = useCachedResources();
-  
+
   if (!isLoadingComplete) {
     return null;
   } else {
@@ -20,7 +20,7 @@ export default function App() {
       <QueryClientProvider client={queryClient}>
         <SafeAreaProvider>
           <NativeBaseProvider>
-            <Navigation colorScheme={colorScheme} />
+            <Navigation />
             <StatusBar />
             <Toast />
           </NativeBaseProvider>
--- kalaka/navigation/index.tsx
+++ kalaka/navigation/index.tsx
@@ -3,43 +3,39 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { FontAwesome } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as React from 'react';
-import { ColorSchemeName, Pressable } from 'react-native';
+import { FontAwesome } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer, DefaultTheme, DarkTheme } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import { ColorSchemeName, Pressable } from "react-native";
 
-import Colors from '../constants/Colors';
-import ModalScreen from '../screens/ModalScreen';
-import NotFoundScreen from '../screens/NotFoundScreen';
-import TabFollowScreen from '../screens/TabFollowScreen';
-import TabMapScreen from '../screens/TabMapScreen';
-import TabContactScreen from '../screens/TabContactsScreen';
-import TabSettingsScreen from '../screens/TabSettingsScreen';
-import LoginScreen from '../screens/LoginScreen';
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
-import LinkingConfiguration from './LinkingConfiguration';
-import { useEffect, useState } from 'react';
-import { getToken } from '../repository';
+import Colors from "../constants/Colors";
+import ModalScreen from "../screens/ModalScreen";
+import NotFoundScreen from "../screens/NotFoundScreen";
+import TabFollowScreen from "../screens/TabFollowScreen";
+import TabMapScreen from "../screens/TabMapScreen";
+import TabContactScreen from "../screens/TabContactsScreen";
+import TabSettingsScreen from "../screens/TabSettingsScreen";
+import LoginScreen from "../screens/LoginScreen";
+import { RootStackParamList, RootTabParamList, RootTabScreenProps } from "../types";
+import LinkingConfiguration from "./LinkingConfiguration";
+import { useEffect, useState } from "react";
+import { getToken } from "../repository";
 
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+export default function Navigation() {
   const [loggedIn, setLoggedIn] = useState(false);
   useEffect(() => {
     (async () => {
       const token = await getToken();
       setLoggedIn(token ? true : false);
     })();
-    
   }, []);
 
-  return (
-    loggedIn === false ? 
-    <LoginScreen callback={setLoggedIn}></LoginScreen> 
-    :
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+  return loggedIn === false ? (
+    <LoginScreen callback={setLoggedIn}></LoginScreen>
+  ) : (
+    <NavigationContainer linking={LinkingConfiguration}>
       <RootNavigator />
     </NavigationContainer>
   );
@@ -55,12 +51,12 @@ function RootNavigator() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: "Oops!" }} />
       {/* <Stack.Screen name="TabMap" component={TabMapScreen} options={{headerShown: false}}/>
       <Stack.Screen name="TabFollow" component={TabFollowScreen} options={{headerShown: false}}/>
       <Stack.Screen name="TabContacts" component={TabContactScreen} options={{headerShown: false}}/>
       <Stack.Screen name="TabSettings" component={TabSettingsScreen} options={{headerShown: false}}/> */}
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
+      <Stack.Group screenOptions={{ presentation: "modal" }}>
         <Stack.Screen name="Modal" component={ModalScreen} />
       </Stack.Group>
     </Stack.Navigator>
@@ -79,33 +75,30 @@ function BottomTabNavigator() {
       initialRouteName="TabFollow"
       screenOptions={{
         tabBarActiveTintColor: Colors.primary,
-      }}>
+      }}
+    >
       <BottomTab.Screen
         name="TabMap"
         component={TabMapScreen}
         options={{
-          title: 'Map',
+          title: "Map",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
       <BottomTab.Screen
         name="TabFollow"
         component={TabFollowScreen}
-        options={({ navigation }: RootTabScreenProps<'TabFollow'>) => ({
-          title: 'Follow',
+        options={({ navigation }: RootTabScreenProps<"TabFollow">) => ({
+          title: "Follow",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Pressable
-              onPress={() => navigation.navigate('Modal')}
+              onPress={() => navigation.navigate("Modal")}
               style={({ pressed }) => ({
                 opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors.text}
-                style={{ marginRight: 15 }}
-              />
+              })}
+            >
+              <FontAwesome name="info-circle" size={25} color={Colors.text} style={{ marginRight: 15 }} />
             </Pressable>
           ),
         })}
@@ -133,9 +126,6 @@ function BottomTabNavigator() {
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
+function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>["name"]; color: string }) {
   return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
 }
