cf6268a2553fe3bc22a8b9d89b030dcec2de8bba
Kecseti Istvan <kecseti.istvan@codespring.ro>
2021-12-11 22:27:56 +0200

    Voice recognition and sms sending

--- kalaka/screens/TabFollowScreen.tsx
+++ kalaka/screens/TabFollowScreen.tsx
@@ -5,12 +5,54 @@ import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
 import { useQuery } from "react-query";
 import { me } from "../services";
-
+import { readText } from '../services/fakeCallService';
+import randomWords from 'random-words';
+import { useEffect } from "react";
+import Toast from "react-native-toast-message";
+import Voice from '@react-native-voice/voice';
+import { Audio } from 'expo-av';
+import { alertContacts } from "../services/alertService";
+import Permissions from 'react-native-permissions';
 export default function TabFollowScreen({ navigation }: RootTabScreenProps<"TabFollow">) {
+  const [shouldPlayVoiceCycle, setShouldPlayVoiceCycle] = React.useState<boolean>(false);
+
+  Voice.onSpeechStart = (e) => {console.log('Speech start')};
+  Voice.onSpeechEnd = (e) => { console.log(e)};
+  Voice.onSpeechResults = (e) => {
+    console.log(e)
+    if(e.value?.includes('apple')) {
+      setShouldPlayVoiceCycle(false)
+    }
+  };
+  
+  const voiceCycleCallback = () => {
+    readText(randomWords({exactly:60, join: ' '}), startVoice)
+  }
+
+  const startVoice = () => {
+    Voice.start('en-US'); 
+    setTimeout(() => { shouldPlayVoiceCycle ? voiceCycleCallback() : () => {} }, 5000)
+  }
+
+  useEffect(() => {
+    (async () => {
+      await Audio.requestPermissionsAsync();
+      await Permissions.request(Permissions.PERMISSIONS.ANDROID.SEND_SMS);
+    })();
+    if(shouldPlayVoiceCycle) {
+      Toast.show({
+        type: 'info',
+        text1: 'Apple',
+        text2: 'A biztonsági szavad'
+      })
+      voiceCycleCallback();
+    }
+  }, [shouldPlayVoiceCycle]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{}</Text>
-      <Button onPress={() => navigation.navigate("Danger")}>Start</Button>
+      <Button onPress={() => setShouldPlayVoiceCycle(true)}>Start cycle</Button>
+      <Button onPress={() => alertContacts()}>Send emergency SMS</Button>
     </View>
   );
 }
---
+++ kalaka/services/alertService.js
@@ -0,0 +1,25 @@
+import SmsAndroid from 'react-native-get-sms-android';
+import { me } from './index'; 
+export const alertContacts = async() => {
+    const loggedInUser = await me();
+    const phoneNumbers = [];
+    loggedInUser.emergencyContacts.forEach(contact =>  {phoneNumbers.push(contact.emergencyInfo)})
+    console.log(phoneNumbers)
+    const lon = loggedInUser.location.longitude;
+    const lat = loggedInUser.location.latitude;
+    const timeStamp = loggedInUser.location.updatedAt;
+    /*
+    phoneNumbers.forEach(phoneNumber => {
+        SmsAndroid.autoSend(
+            phoneNumber,
+            `Bajban vagyok! Kérlek segíts, utolsó rögzített pozicióm: https://www.google.com/maps/search/?api=1&query=${lat},${lon} , ekkor: ${timeStamp}!`,
+            (fail) => {
+              console.log('Failed with this error: ' + fail);
+            },
+            (success) => {
+              console.log('SMS sent successfully');
+            },
+          );
+    })
+   */
+}
